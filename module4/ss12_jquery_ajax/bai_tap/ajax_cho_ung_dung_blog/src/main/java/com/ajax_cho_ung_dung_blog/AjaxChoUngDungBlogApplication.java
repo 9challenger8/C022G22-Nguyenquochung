42d4a70fd4ee6654975072496c6c17da911eecb4
unknown <9challenger8@gmail.com>
2022-07-04 09:04:42 +0700

    up

---
+++ module4/ss12_jquery_ajax/bai_tap/ajax_cho_ung_dung_blog/src/main/java/com/ajax_cho_ung_dung_blog/AjaxChoUngDungBlogApplication.java
@@ -0,0 +1,13 @@
+package com.ajax_cho_ung_dung_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxChoUngDungBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjaxChoUngDungBlogApplication.class, args);
+    }
+
+}
---
+++ module4/ss12_jquery_ajax/bai_tap/ajax_cho_ung_dung_blog/src/test/java/com/ajax_cho_ung_dung_blog/AjaxChoUngDungBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajax_cho_ung_dung_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxChoUngDungBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
