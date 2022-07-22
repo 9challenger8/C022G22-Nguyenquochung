5b3e85620945163f2a9a821b8ea9a4f0897e9521
unknown <9challenger8@gmail.com>
2022-07-22 16:46:03 +0700

    origin main

--- module5/casestudy-module5-angular/src/app/customer/customer-list/customer-list.component.ts
+++ module5/casestudy-module5-angular/src/app/customer/customer-list/customer-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerListComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ module5/ss6_angular_form/bai-tap-ss6/src/app/register/register.component.ts
@@ -0,0 +1,45 @@
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup;
+  formPassword: FormGroup;
+
+
+  constructor() {
+    this.registerForm = new FormGroup({
+      email: new FormControl("", [Validators.required, Validators.email]),
+      formPassword: new FormGroup({
+        password: new FormControl("", [Validators.minLength(6), Validators.required]),
+        confirmPassword: new FormControl("", this.validateConfirm)
+      }),
+      country: new FormControl('', [Validators.required]),
+      gender: new FormControl('', [Validators.required]),
+      age: new FormControl('', [Validators.min(18), Validators.required]),
+      phone: new FormControl('', [Validators.required, Validators.pattern(/^\+84\d{9,10}$/)])
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  registerFormSuccess() {
+    console.log('Ok')
+  }
+
+  validateConfirm(confirmPassword: AbstractControl) {
+    let value1 = confirmPassword.value
+    let value2 = this.registerForm.controls.password.value
+    if (value1 == value2) {
+      return {'checkPassword': true};
+    }
+    return null;
+  }
+
+}
